33190a634efd7582582171b4c0f112bcf22c8ce5
Landon <landonkazen@gmail.com>
2026-01-30 11:38:51 -0500

    added variables and implemented incrementCount to home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,13 +10,30 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _counter = 0;
+  final int MAX_COUNT = 2000000000;
+  int count = 0;
+  int amountGained = 1;
+  int clickerCount = 0;
+  int clickerCost = 100;
+  int multiplierCost = 150;
+  int clickerMultiplierCost = 1000;
+  int clickerGain = 1;
+  late int clickerInterval; // single interval for all clickers
+  bool confirmReset = false;
+  bool loaded = false;
+  late int saveInterval;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+
+  // functions for app logic
+  void incrementCount() {
+    if (count + amountGained <= MAX_COUNT) {
+      setState(() {
+        count += amountGained;
+      });
+    }
   }
+  
+
 
   @override
   Widget build(BuildContext context) {
@@ -31,16 +48,11 @@ class _HomePageState extends State<HomePage> {
           children: [
             const Text('You have pushed the button this many times:'),
             Text(
-              '$_counter',
+              '$count',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
